c2377d22063417b1ace1cb82dedf03825d30995f
Lars Vierbergen <lars.vierbergen@xenit.eu>
2019-03-30 12:00:13 +0100

    Add checks for amp and jar file contents to the integration test

--- src/integration-test/java/eu/xenit/gradle/alfrescosdk/AbstractIntegrationTest.java
+++ src/integration-test/java/eu/xenit/gradle/alfrescosdk/AbstractIntegrationTest.java
@@ -9,51 +9,40 @@ import org.apache.commons.io.FileUtils;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
 import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
 /**
  * Created by thijs on 3/2/17.
  */
 public abstract class AbstractIntegrationTest {
-    protected void testProjectFolderThatShouldFail(Path projectFolder, String task) throws IOException {
-        testProjectFolder(projectFolder, task, true);
-    }
-    protected void testProjectFolder(Path projectFolder, String task)throws IOException {
-        testProjectFolder(projectFolder,task,false);
-    }
-    protected void testProjectFolder(Path projectFolder, String task, boolean expectsException) throws IOException {
-        TemporaryFolder temporaryFolder = new TemporaryFolder();
-        temporaryFolder.create();
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
 
-        try {
-            File tempExample = temporaryFolder.newFolder(projectFolder.getFileName().toString());
-            FileUtils.copyDirectory(projectFolder.toFile(), tempExample);
-            System.out.println("Executing test build for example " + tempExample.getName());
-            BuildResult result = GradleRunner.create()
-                    .withProjectDir(tempExample)
-                    .withArguments(task, "--stacktrace", "--rerun-tasks", "-i")
-                    .withTestKitDir(new File(System.getProperty("user.home") + File.separator + ".gradle"))
-                    .withPluginClasspath()
-                    .withDebug(true)
-                    .forwardOutput()
-                    .build();
+    public File projectFolder;
 
-            if(expectsException){
-                assert false;
-            }else{
-                assertEquals(TaskOutcome.SUCCESS, result.task(task).getOutcome());
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-            assert expectsException;
-        }
-
-        finally {
-            temporaryFolder.delete();
-        }
+    protected BuildResult testProjectFolderThatShouldFail(Path projectFolder, String task) throws IOException {
+        return createRunner(projectFolder, task).buildAndFail();
     }
 
-    protected void testProjectFolder(Path projectFolder) throws IOException {
-        testProjectFolder(projectFolder, ":amp");
+    private GradleRunner createRunner(Path projectFolder, String task) throws IOException {
+        File tempExample = testFolder.newFolder(projectFolder.getFileName().toString());
+        this.projectFolder = tempExample;
+        FileUtils.copyDirectory(projectFolder.toFile(), tempExample);
+        System.out.println("Executing test build for example " + tempExample.getName());
+        return GradleRunner.create()
+                .withProjectDir(tempExample)
+                .withArguments(task, "--stacktrace", "--rerun-tasks", "-i")
+                .withPluginClasspath()
+                .withDebug(true)
+                .forwardOutput();
+    }
+
+    protected BuildResult testProjectFolder(Path projectFolder, String task) throws IOException {
+        return createRunner(projectFolder, task).build();
+    }
+
+    protected BuildResult testProjectFolder(Path projectFolder) throws IOException {
+        return testProjectFolder(projectFolder, ":amp");
     }
 }
--- src/integration-test/java/eu/xenit/gradle/alfrescosdk/Examples.java
+++ src/integration-test/java/eu/xenit/gradle/alfrescosdk/Examples.java
@@ -1,21 +1,72 @@
 package eu.xenit.gradle.alfrescosdk;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Predicate;
 import org.junit.Test;
 
 public class Examples extends AbstractIntegrationTest {
 
     private static Path EXAMPLES = Paths.get("src", "integration-test", "resources", "examples");
 
+    private static void assertPath(Predicate<Path> check, Path path) {
+        assertTrue(path.toString(), check.test(path));
+    }
+
     @Test
     public void testSimpleAlfrescoProject() throws IOException {
         testProjectFolder(EXAMPLES.resolve("simple-alfresco-project"));
+
+        Path buildFolder = projectFolder.toPath().resolve("build");
+
+        Path ampFile = buildFolder.resolve("dist/simple-alfresco-project-0.0.1.amp");
+        Path jarFile = buildFolder.resolve("libs/simple-alfresco-project-0.0.1.jar");
+
+        assertPath(Files::isRegularFile, ampFile);
+        assertPath(Files::isRegularFile, jarFile);
+
+        FileSystem ampFs = FileSystems.newFileSystem(ampFile, null);
+        assertPath(Files::isRegularFile, ampFs.getPath("module.properties"));
+        assertPath(Files::isRegularFile,ampFs.getPath("config/alfresco/extension/templates/webscripts/eu/xenit/alfresco/gradle/sample/helloworld.get.desc.xml"));
+        assertPath(Files::isRegularFile,ampFs.getPath("config/alfresco/module/example-default-amp/module-context.xml"));
+        Path packagedJarFile = ampFs.getPath("lib/simple-alfresco-project-0.0.1.jar");
+        assertPath(Files::isRegularFile, packagedJarFile);
+        assertArrayEquals("Jar inside amp is not identical to jar outside amp", Files.readAllBytes(jarFile), Files.readAllBytes(packagedJarFile));
+        ampFs.close();
+
+        FileSystem jarFs = FileSystems.newFileSystem(jarFile, null);
+        assertPath(Files::isRegularFile, jarFs.getPath("eu/xenit/alfresco/gradle/sample/HelloWorldWebScript.class"));
+        jarFs.close();
     }
 
     @Test
     public void testSimpleDeProject() throws IOException {
         testProjectFolder(EXAMPLES.resolve("simple-de-project"));
+
+        Path buildFolder = projectFolder.toPath().resolve("build");
+
+        Path ampFile = buildFolder.resolve("dist/simple-de-project-0.0.1.amp");
+        Path jarFile = buildFolder.resolve("libs/simple-de-project-0.0.1.jar");
+
+        assertPath(Files::isRegularFile, ampFile);
+        assertPath(Files::isRegularFile, jarFile);
+
+        FileSystem ampFs = FileSystems.newFileSystem(ampFile, null);
+        assertPath(Files::isRegularFile, ampFs.getPath("module.properties"));
+        Path packagedJarFile = ampFs.getPath("config/dynamic-extensions/bundles/simple-de-project-0.0.1.jar");
+        assertPath(Files::isRegularFile, packagedJarFile);
+        assertArrayEquals("Jar inside amp is not identical to jar outside amp", Files.readAllBytes(jarFile), Files.readAllBytes(packagedJarFile));
+        ampFs.close();
+
+        FileSystem jarFs = FileSystems.newFileSystem(jarFile, null);
+        assertPath(Files::isRegularFile, jarFs.getPath("eu/xenit/alfresco/gradle/sample/HelloWorldWebScript.class"));
+        jarFs.close();
     }
 }
