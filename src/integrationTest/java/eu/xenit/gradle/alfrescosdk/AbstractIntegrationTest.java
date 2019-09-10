3c2789146b2c0bf7db9ecb4af06172cb8c30766c
Thijs Lemmens <thijs.lemmens@xenit.eu>
2019-09-10 14:56:49 +0200

    Updating Gradle versions

--- src/integrationTest/java/eu/xenit/gradle/alfrescosdk/AbstractIntegrationTest.java
+++ src/integrationTest/java/eu/xenit/gradle/alfrescosdk/AbstractIntegrationTest.java
@@ -28,6 +28,9 @@ public abstract class AbstractIntegrationTest {
     @Parameters(name = "Gradle v{0}")
     public static Collection<Object[]> testData() {
         return Arrays.asList(new Object[][]{
+                {"5.6.2"},
+                {"5.5.1"},
+                {"5.4.1"},
                 {"5.3.1"},
                 {"5.2.1"},
                 {"5.1.1"},
