76b1e97ba56893b2445cb3a5d77cfa3cc92582a7
Thijs Lemmens <thijs.lemmens@xenit.eu>
2019-09-10 13:43:29 +0200

    Updated GradleVersionCheck: new minimal version 5.0

--- src/main/java/eu/xenit/gradle/alfrescosdk/internal/GradleVersionCheck.java
+++ src/main/java/eu/xenit/gradle/alfrescosdk/internal/GradleVersionCheck.java
@@ -7,15 +7,13 @@ public final class GradleVersionCheck {
     private GradleVersionCheck() {
 
     }
-    private static final GradleVersion MINIMUM_VERSION = GradleVersion.version("4.10");
+    private static final GradleVersion MINIMUM_VERSION = GradleVersion.version("5.0");
     public static void assertSupportedVersion(String pluginId) {
         GradleVersion currentVersion = GradleVersion.current();
 
         if(MINIMUM_VERSION.compareTo(currentVersion) > 0) {
             throw new GradleException("The "+pluginId+" plugin requires at least "+MINIMUM_VERSION+". (You are running "+currentVersion+")");
         }
-
-        //TODO: Check for newer Gradle versions and show relavent errors instead.
     }
 
 }
